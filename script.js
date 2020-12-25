c26c19258dae254de65d38eb6ab6ff925e861c6e
MaksimZubrytski <69052702+MaksimZubrytski@users.noreply.github.com>
2020-12-25 13:53:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,42 @@
 let library = new Proxy([
-    { label: 'красная шапочка'},
-    { label: 'колобок'},
-    { label: 'теремок'}
+  { label: "красная шапочка"},
+  { label: "колобок"},
+  { label: "теремок"}
   ],
   {
     get: function(array, prop) {
-    // Получение книги по числу
+        // Получение книги по числу
       if (prop in array) {
         return array[prop];
       }
-  
       let result;
-  
+
       for (let book of array) {
         if (book.label === prop) {
           result = book.label;
+          break;
         }
       }
-      // Получение книги по имени
+        // Получение книги по имени
       if (result) {
-        return result.split(' ').map(letter => letter[0].toUpperCase() + letter.substring(1)).join(' ')
+        return result.split(" ")
+        .map(letter => letter[0].toUpperCase() + letter.substring(1))
+        .join(" ");
       }
-      else {
-          return alert('Книга отсутствует')
-      }
+
+      return alert("Книга отсутствует");
+
     },
     set(array, prop, value) {
-        if (typeof value === 'string') {
-            let nameBookSmallLetter = value.toLowerCase();
-            array[prop] = {label: nameBookSmallLetter};
-            return true;
-          } else {
-            return value;
-          }
-        }
+
+      if (typeof value === "string") {
+        const bookLabelInSmallLetters = value.toLowerCase();
+        array[prop] = {label: bookLabelInSmallLetters};
+        return true;
+      } else {
+        return value;
+      }
     }
-  );
+  }
+);
 
